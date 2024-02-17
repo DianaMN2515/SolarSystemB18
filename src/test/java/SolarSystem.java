2a01c4362970fdff6abe73b961d88f32440b3c78
DianaMN2515 <dgermanovsky@gmail.com>
2024-02-17 12:41:51 -0500

    i have worked on planet2

--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -13,5 +13,10 @@ public class SolarSystem {
         frog.color="blue";
         frog.temp="-123213124k";
         frog.size="2km";
+
+        Planet2 gecko =new Planet2();
+        gecko.color="magenta";
+        gecko.temp="34343443434k";
+        gecko.size="342344342m";
     }
 }
